bf5669a6db674304ec6a134f9bff47c10d84290a
Abu Abdullah <abdullah72308@gmail.com>
2023-02-18 04:53:08 +0600

    calculation complete

---
+++ dom.js
@@ -0,0 +1,16 @@
+        // JavaScript code
+        // function getRandomColor() {
+        // return Math.floor(Math.random() * 20) + 2;
+        // }
+
+        // document.querySelectorAll('.card').forEach(function(card) {
+        // card.addEventListener('mouseenter', function() {
+        //     card.classList.add(`bg-random-${getRandomColor()}`);
+        // });
+        // card.addEventListener('mouseleave', function() {
+        //     card.classList.remove(`bg-random-${getRandomColor()}`);
+        // });
+        // });
+
+
+        
